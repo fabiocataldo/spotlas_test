aae5e27fca6a931b69355c96dc4618366e26e8fe
fabiocataldo <fabiocataldo@gmail.com>
2022-06-10 13:12:12 +0100

    final organization

--- lib/static/static.dart
+++
@@ -1,6 +0,0 @@
-class StaticValues {
-  const StaticValues._();
-
-  static const String apiUrl =
-      "https://dev.api.spotlas.com/interview/feed?page=1";
-}
--- lib/themes/themes.dart
+++ lib/themes/themes.dart
@@ -18,32 +18,6 @@ mixin CustomTheme implements ThemeData {
               ),
             ],
           ),
-          headline2: TextStyle(
-              fontSize: 36.0, fontStyle: FontStyle.italic, color: Colors.white),
-          bodyText1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
         ));
   }
 }
-// ThemeData darkTheme = ThemeData(
-//     brightness: Brightness.dark,
-//     backgroundColor: Colors.grey[700],
-//     accentColor: Colors.white,
-//   );
-
-//   static List<ThemeData> _appThemes = [
-//     ///Theme 1
-//     ThemeData(
-//         textSelectionHandleColor: Colors.white,
-//         selectedRowColor: Colors.green
-// ),
-//    ///Theme 2
-//     ThemeData(
-//         textSelectionHandleColor: Colors.white,
-//         selectedRowColor: Colors.green
-// ),
-//    ///Theme 3
-//     ThemeData(
-//         textSelectionHandleColor: Colors.white,
-//         selectedRowColor: Colors.green
-// ),
-// ]
